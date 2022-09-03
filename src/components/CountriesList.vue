8da27ec2b83b28459231a7e459980ba9012995f0
Mae Gallegoe <maegallego97@gmail.com>
2022-09-03 22:47:50 +0200

    solved

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<router-link
+			v-for="country in countries"
+			:key="country.id"
+			:to="`${country.alpha3Code}`"
+		>
+			<div class="show-country">
+				<img
+					:src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`"
+					:alt="`Flag of ${country.name}`"
+				/>
+				<p>{{ country.name }}</p>
+			</div>
+		</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "CountriesList",
+		props: {
+			countries: Array,
+		},
+	};
+</script>
+
+<style>
+	.show-country {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: 12px;
+		border: 1px solid #656565;
+	}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">WikiCountries</div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style>
+.navbar {
+  width: 100%;
+  background-color: blue;
+  color: white;
+}
+</style>
